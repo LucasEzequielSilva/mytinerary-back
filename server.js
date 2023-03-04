823ac266adbcf77ccde80cfbf5ac40758274a5bb
unknown <luxassilva@gmail.com>
2023-03-04 16:25:53 -0300

    1.0.0

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+require('./config/database')
+const Router = require('./routes/routes')
+const port = 8080
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', Router)
+
+
+app.listen(port, ()=>console.log('server ready on port' + port))
